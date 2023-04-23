ae5b720cc270225502bc7d823681593c76f8b90d
Sanket Gawande <sanketgawande.gcoey@gmail.com>
2023-04-23 09:09:01 +0000

    Updates

---
+++ precache-manifest.b3bd8ce061abbee49fb805a3bcae3ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef2be3e2686384891323",
+    "url": "/react-router/static/css/main.d21cb16e.chunk.css"
+  },
+  {
+    "revision": "ef2be3e2686384891323",
+    "url": "/react-router/static/js/main.ef2be3e2.chunk.js"
+  },
+  {
+    "revision": "f3784682a4abd8095750",
+    "url": "/react-router/static/js/1.f3784682.chunk.js"
+  },
+  {
+    "revision": "09b5b4453bf0f46fba71",
+    "url": "/react-router/static/js/2.09b5b445.chunk.js"
+  },
+  {
+    "revision": "101d0056a602ea02bcc1",
+    "url": "/react-router/static/js/3.101d0056.chunk.js"
+  },
+  {
+    "revision": "e5078efeda90fc7aef67",
+    "url": "/react-router/static/js/runtime~main.e5078efe.js"
+  },
+  {
+    "revision": "7a2bd64686b3df5e701ff185fb279c02",
+    "url": "/react-router/index.html"
+  }
+];
